5082c7f2b70beb8ca8942b8f1ae5062dac3ea5c7
karur4n <karuran.f@gmail.com>
2014-12-24 13:02:34 +0900

    Try TableLayout

--- app/src/main/java/com/example/karuran/android_lesson/MainActivity.java
+++ app/src/main/java/com/example/karuran/android_lesson/MainActivity.java
@@ -13,16 +13,26 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        LinearLayout ll = new LinearLayout(this);
-        ll.setOrientation(LinearLayout.VERTICAL);
-        setContentView(ll);
+        TableLayout tl = new TableLayout(this);
+        setContentView(tl);
         
+        TableRow[] tr = new TableRow[10];
+        
+        TextView[] tv = new TextView[10];
         Button[] bt = new Button[10];
         
-        for(int i=0; i<bt.length; i++) {
+        for(int i=0; i<tr.length; i++) {
+            tr[i] = new TableRow(this);
+            
+            tv[i] = new TextView(this);
+            tv[i].setText("商品番号" + i + "-------");
             bt[i] = new Button(this);
-            bt[i].setText(Integer.toString(i));
-            ll.addView(bt[i]);
+            bt[i].setText("購入"); 
+            
+            tr[i].addView(tv[i]);
+            tr[i].addView(bt[i]);
+            
+            tl.addView(tr[i]);
         }
     }
 
